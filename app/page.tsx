d993d8d39ded5e9d45ce36e543ac44bc82ea3ac9
Lise Carpenter <lise.carpenter@gmail.com>
2024-08-08 22:03:22 +0700

    add calendar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import Image from 'next/image'
 import GroupPhoto from '~public/images/bwgbangkok-group.webp'
 import GroupPhoto2 from '~public/images/bwg-group-photo.webp'
 import { Cedarville_Cursive, Montserrat } from 'next/font/google'
-import Example from '~components/header_2'
+import Header from '~components/header'
 import Logo from '~public/images/logo-no-bg.png'
 import { InformationCircleIcon } from '@heroicons/react/16/solid'
+import Calendar from '~components/calendar'
+import Footer from '~components/footer'
 
 const cedarville = Cedarville_Cursive({
   weight: '400',
@@ -20,7 +22,7 @@ export default function Home() {
     <>
       <main className={`${montserrat.className} bg-transparent`}>
         <section className='top-0 w-full z-50 fixed'>
-          <Example />
+          <Header />
         </section>
         <section className='h-screen flex items-center justify-center'>
           <div className='relative w-full h-screen'>
@@ -50,106 +52,112 @@ export default function Home() {
             </h3>
           </div>
         </section>
-        <div className='relative isolate overflow-hidden bg-white py-24 sm:py-32'>
-          <div
-            aria-hidden='true'
-            className='absolute -top-80 left-[max(6rem,33%)] -z-10 transform-gpu blur-3xl sm:left-1/2 md:top-20 lg:ml-20 xl:top-3 xl:ml-56'>
+        <section>
+          <div className='relative isolate overflow-hidden bg-white py-24 sm:py-32'>
             <div
-              style={{
-                clipPath:
-                  'polygon(63.1% 29.6%, 100% 17.2%, 76.7% 3.1%, 48.4% 0.1%, 44.6% 4.8%, 54.5% 25.4%, 59.8% 49.1%, 55.3% 57.9%, 44.5% 57.3%, 27.8% 48%, 35.1% 81.6%, 0% 97.8%, 39.3% 100%, 35.3% 81.5%, 97.2% 52.8%, 63.1% 29.6%)',
-              }}
-              className='aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#3b82f6] opacity-30'
-            />
-          </div>
-          <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-            <div className='mx-auto max-w-2xl lg:mx-0'>
-              <p className='text-lg font-semibold leading-8 tracking-tight text-blue-500'>
-                Build lasting connections
-              </p>
-              <h1 className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-                A Community for Women
-              </h1>
-              <p className='mt-6 text-xl leading-8 text-gray-700'>
-                Since 1969, the British Women&apos;s Group has served as a
-                welcoming community for women, whether newcomers or long term
-                residents.
-              </p>
+              aria-hidden='true'
+              className='absolute -top-80 left-[max(6rem,33%)] -z-10 transform-gpu blur-3xl sm:left-1/2 md:top-20 lg:ml-20 xl:top-3 xl:ml-56'>
+              <div
+                style={{
+                  clipPath:
+                    'polygon(63.1% 29.6%, 100% 17.2%, 76.7% 3.1%, 48.4% 0.1%, 44.6% 4.8%, 54.5% 25.4%, 59.8% 49.1%, 55.3% 57.9%, 44.5% 57.3%, 27.8% 48%, 35.1% 81.6%, 0% 97.8%, 39.3% 100%, 35.3% 81.5%, 97.2% 52.8%, 63.1% 29.6%)',
+                }}
+                className='aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#3b82f6] opacity-30'
+              />
             </div>
-            <div className='mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:mt-10 lg:max-w-none lg:grid-cols-12'>
-              <div className='relative lg:order-last lg:col-span-5'>
-                <svg
-                  aria-hidden='true'
-                  className='absolute -top-[40rem] left-1 -z-10 h-[64rem] w-[175.5rem] -translate-x-1/2 stroke-gray-900/10 [mask-image:radial-gradient(64rem_64rem_at_111.5rem_0%,white,transparent)]'>
-                  <defs>
-                    <pattern
-                      id='e87443c8-56e4-4c20-9111-55b82fa704e3'
-                      width={200}
-                      height={200}
-                      patternUnits='userSpaceOnUse'>
-                      <path d='M0.5 0V200M200 0.5L0 0.499983' />
-                    </pattern>
-                  </defs>
-                  <rect
-                    fill='url(#e87443c8-56e4-4c20-9111-55b82fa704e3)'
-                    width='100%'
-                    height='100%'
-                    strokeWidth={0}
-                  />
-                </svg>
-                <figure className='border-l border-blue-500 pl-8'>
-                  <blockquote className='text-xl font-semibold leading-8 tracking-tight text-gray-900'>
-                    <p>
-                      “After spending more than 30 years in Bangkok, I joined
-                      the BWG . I&apos;ve made lots of new friendships and
-                      rekindled old ones, and have had a lot of fun along the
-                      way. The BWG is a friendly, welcoming group”
-                    </p>
-                  </blockquote>
-                  <figcaption className='mt-8 flex gap-x-4 items-center'>
-                    <img
-                      alt=''
-                      src='https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                      className='mt-1 h-10 w-10 flex-none rounded-full bg-gray-50'
-                    />
-                    <div className='text-sm leading-6'>
-                      <div className='font-semibold text-gray-900'>Karen</div>
-                    </div>
-                  </figcaption>
-                </figure>
-              </div>
-              <div className='max-w-xl text-base leading-7 text-gray-700 lg:col-span-7'>
-                <p>
-                  By joining our vibrant group you will discover new
-                  friendships, build lasting connections, and participate in
-                  engaging activities. Women of all age and nationalities are
-                  warmly welcomed. Join us and meet like-minded women, enjoy
-                  social activities, fun nights out and get involved in fund
-                  raising activities.
-                  <strong className='block mt-5 font-bold text-red-300'>
-                    Come and join the fun!
-                  </strong>
+            <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+              <div className='mx-auto max-w-2xl lg:mx-0'>
+                <p className='text-lg font-semibold leading-8 tracking-tight text-blue-500'>
+                  Build lasting connections
                 </p>
-                <figure className='mt-16'>
-                  <Image
-                    alt='group photo'
-                    src={GroupPhoto2}
-                    width={576}
-                    height={364}
-                    className='aspect-video rounded-xl bg-gray-50 object-cover'
-                  />
-                  <figcaption className='mt-4 flex gap-x-2 text-sm leading-6 text-gray-500'>
-                    <InformationCircleIcon
-                      aria-hidden='true'
-                      className='mt-0.5 h-5 w-5 flex-none text-gray-300'
+                <h1 className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                  A Community for Women
+                </h1>
+                <p className='mt-6 text-xl leading-8 text-gray-700'>
+                  Since 1969, the British Women&apos;s Group has served as a
+                  welcoming community for women, whether newcomers or long term
+                  residents.
+                </p>
+              </div>
+              <div className='mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:mt-10 lg:max-w-none lg:grid-cols-12'>
+                <div className='relative lg:order-last lg:col-span-5'>
+                  <svg
+                    aria-hidden='true'
+                    className='absolute -top-[40rem] left-1 -z-10 h-[64rem] w-[175.5rem] -translate-x-1/2 stroke-gray-900/10 [mask-image:radial-gradient(64rem_64rem_at_111.5rem_0%,white,transparent)]'>
+                    <defs>
+                      <pattern
+                        id='e87443c8-56e4-4c20-9111-55b82fa704e3'
+                        width={200}
+                        height={200}
+                        patternUnits='userSpaceOnUse'>
+                        <path d='M0.5 0V200M200 0.5L0 0.499983' />
+                      </pattern>
+                    </defs>
+                    <rect
+                      fill='url(#e87443c8-56e4-4c20-9111-55b82fa704e3)'
+                      width='100%'
+                      height='100%'
+                      strokeWidth={0}
                     />
-                    Group photo from x event
-                  </figcaption>
-                </figure>
+                  </svg>
+                  <figure className='border-l border-blue-500 pl-8'>
+                    <blockquote className='text-xl font-semibold leading-8 tracking-tight text-gray-900'>
+                      <p>
+                        “After spending more than 30 years in Bangkok, I joined
+                        the BWG . I&apos;ve made lots of new friendships and
+                        rekindled old ones, and have had a lot of fun along the
+                        way. The BWG is a friendly, welcoming group”
+                      </p>
+                    </blockquote>
+                    <figcaption className='mt-8 flex gap-x-4 items-center'>
+                      <img
+                        alt=''
+                        src='https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                        className='mt-1 h-10 w-10 flex-none rounded-full bg-gray-50'
+                      />
+                      <div className='text-sm leading-6'>
+                        <div className='font-semibold text-gray-900'>Karen</div>
+                      </div>
+                    </figcaption>
+                  </figure>
+                </div>
+                <div className='max-w-xl text-base leading-7 text-gray-700 lg:col-span-7'>
+                  <p>
+                    By joining our vibrant group you will discover new
+                    friendships, build lasting connections, and participate in
+                    engaging activities. Women of all age and nationalities are
+                    warmly welcomed. Join us and meet like-minded women, enjoy
+                    social activities, fun nights out and get involved in fund
+                    raising activities.
+                    <strong className='block mt-5 font-bold text-red-300'>
+                      Come and join the fun!
+                    </strong>
+                  </p>
+                  <figure className='mt-16'>
+                    <Image
+                      alt='group photo'
+                      src={GroupPhoto2}
+                      width={576}
+                      height={364}
+                      className='aspect-video rounded-xl bg-gray-50 object-cover'
+                    />
+                    <figcaption className='mt-4 flex gap-x-2 text-sm leading-6 text-gray-500'>
+                      <InformationCircleIcon
+                        aria-hidden='true'
+                        className='mt-0.5 h-5 w-5 flex-none text-gray-300'
+                      />
+                      Group photo from x event
+                    </figcaption>
+                  </figure>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
+        <section className='container mx-auto py-20'>
+          <Calendar />
+        </section>
+        <Footer />
       </main>
     </>
   )
