ca7f874d99b0f422bb146a7a5ce67bace7a30f10
Lise Carpenter <lise.carpenter@gmail.com>
2024-08-11 08:47:29 +0700

    WIP

--- app/application-form/page.tsx
+++ app/application-form/page.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '~components/footer'
 import Header from '~components/header'
+import JoinUs from '~components/join_us'
+import spdf from 'simple-react-pdf'
 
 const About: NextPage<any> = (props) => {
   return (
@@ -16,6 +19,12 @@ const About: NextPage<any> = (props) => {
         /> */}
       </Head>
       <Header />
+      <JoinUs />
+      <a href='public/applicationform.pdf' download='cv'>
+        Download CV
+      </a>
+      <spdf.SimplePDF file='./example1.pdf' />
+
       <Footer />
     </>
   )
