f4521e98ef7e1daa2112ecc144c46aad2ef2fb53
Lise Carpenter <lise.carpenter@gmail.com>
2024-08-11 08:47:10 +0700

    add link

--- components/header.tsx
+++ components/header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
           ))}
         </div>
         <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
-          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
+          <a href='/application-form' className='text-sm font-semibold leading-6 text-gray-900'>
             Join BWG <span aria-hidden='true'>&rarr;</span>
           </a>
         </div>
@@ -95,7 +95,7 @@ export default function Header() {
               </div>
               <div className='py-6'>
                 <a
-                  href='#'
+                  href='/application-form'
                   className='-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
                   Join BWG
                 </a>
