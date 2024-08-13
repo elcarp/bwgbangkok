2b3dfa71539672726ee9cd3e45b480bf49f1221f
Lise Carpenter <lise.carpenter@gmail.com>
2024-08-13 09:26:31 +0700

    styling

--- components/header.tsx
+++ components/header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
     <header className='bg-white opacity-80 shadow-xl'>
       <nav
         aria-label='Global'
-        className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'>
+        className='mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8'>
         <div className='flex lg:flex-1'>
           <a href='/' className='-m-1.5 p-1.5'>
             <span className='sr-only'>Your Company</span>
