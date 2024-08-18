87a278682db7dabdeff6a835d87ae715ca13a612
Lise Carpenter <lise.carpenter@gmail.com>
2024-08-18 11:23:44 +0700

    add text to contac tpage

--- components/contact.tsx
+++ components/contact.tsx
@@ -55,9 +55,9 @@ export default function Example() {
               Get in touch
             </h2>
             <p className='mt-6 text-lg leading-8 text-gray-600'>
-              Proin volutpat consequat porttitor cras nullam gravida at. Orci
-              molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
-              Arcu sed malesuada et magna.
+              Whether you are interested in joining or have any questions for
+              us, please feel free to get in touch. Looking forward to hearing
+              from you!
             </p>
             <dl className='mt-10 space-y-4 text-base leading-7 text-gray-600'>
               {/* <div className="flex gap-x-4">
