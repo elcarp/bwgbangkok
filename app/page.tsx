65e2ee49628821101c619bde4e56da30858b3c2d
Lise Carpenter <lise.carpenter@gmail.com>
2024-09-01 13:57:45 +0700

    add keen slider and events carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import GroupPhoto from '~public/images/bwgbangkok-group.webp'
 import GroupPhoto2 from '~public/images/bwg-group-photo.webp'
@@ -8,13 +10,51 @@ import Footer from '~components/footer'
 import CombinedLunch from '~public/images/combined_lunch.webp'
 import ExpatsBangkok from '~public/images/expats_bangkok.webp'
 import { Cedarville_Cursive } from 'next/font/google'
-import Head from 'next/head'
 const cedarville = Cedarville_Cursive({
   weight: '400',
   subsets: ['latin'],
 })
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+import React, { useState } from 'react'
 
 export default function Home() {
+  const [currentSlide, setCurrentSlide] = React.useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
+
+  function Arrow(props: {
+    disabled: boolean
+    left?: boolean
+    onClick: (e: any) => void
+  }) {
+    const disabled = props.disabled ? ' arrow--disabled' : ''
+    return (
+      <svg
+        onClick={props.onClick}
+        className={`arrow ${
+          props.left ? 'arrow--left' : 'arrow--right'
+        } ${disabled}`}
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'>
+        {props.left && (
+          <path d='M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z' />
+        )}
+        {!props.left && (
+          <path d='M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z' />
+        )}
+      </svg>
+    )
+  }
   return (
     <>
       <main className={`bg-transparent`}>
@@ -168,7 +208,7 @@ export default function Home() {
         <section className='container mx-auto py-20'>
           <Calendar />
         </section>
-        <section>
+        <section className='py-20'>
           <div className='container mx-auto lg:flex items-center'>
             <div className='w-full lg:w-1/2 mx-auto h-screen-3/4 shadow-xl relative rounded-xl'>
               <Image
@@ -215,6 +255,80 @@ export default function Home() {
               />
             </div>
           </div>
+        </section>
+        <section>
+          <div className='navigation-wrapper h-screen-1/2'>
+            <div ref={sliderRef} className='keen-slider h-screen-1/2'>
+              <div className='keen-slider__slide number-slide1'>
+                <img
+                className='h-full'
+                  src='https://67kbtiuxase3xqul.public.blob.vercel-storage.com/e3c12357-a09e-4f66-aa60-975b98753f34-my41XAHbjfCjOcQdiJH4DFJQFdTuYh.jpeg'
+                  alt='bow making session'
+                />
+              </div>
+              <div className='keen-slider__slide number-slide1'>
+                <img
+                className='h-full'
+                  src='https://67kbtiuxase3xqul.public.blob.vercel-storage.com/e30fb2c7-4db8-44cd-8528-785d5768970f-eoDKVFGW3tER3jCDIbj3wiiuWAah9e.jpeg'
+                  alt='coffee morning'></img>
+              </div>
+              <div className='keen-slider__slide number-slide1'>
+                <img
+                className='h-full'
+                  src='https://67kbtiuxase3xqul.public.blob.vercel-storage.com/09bba56b-3e67-4a1f-9d2d-792f7ca89637-vKE0oFe4SJHHP3ehAQOkWu4hsy4265.jpeg'
+                  alt='lunch buffet'
+                />
+              </div>
+              <div className='keen-slider__slide number-slide1'>
+                <img
+                className='h-full'
+                  src='https://67kbtiuxase3xqul.public.blob.vercel-storage.com/6d63646a-94e2-44d8-8d71-89a327f5fd2c-g15pDBL3a0rXJHiK9VHx5QBgEBkVYP.jpeg'
+                  alt='origami workshop'
+                />
+              </div>
+            </div>
+            {loaded && instanceRef.current && (
+              <>
+                <Arrow
+                  left
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.prev()
+                  }
+                  disabled={currentSlide === 0}
+                />
+
+                <Arrow
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.next()
+                  }
+                  disabled={
+                    currentSlide ===
+                    instanceRef.current.track.details.slides.length - 1
+                  }
+                />
+              </>
+            )}
+          </div>
+          {loaded && instanceRef.current && (
+            <div className='dots'>
+              {[
+                ...(Array(
+                  instanceRef.current.track.details.slides.length
+                ).keys() as any),
+              ].map((idx) => {
+                return (
+                  <button
+                    key={idx}
+                    onClick={() => {
+                      instanceRef.current?.moveToIdx(idx)
+                    }}
+                    className={
+                      'dot' + (currentSlide === idx ? ' active' : '')
+                    }></button>
+                )
+              })}
+            </div>
+          )}
         </section>
         <Footer />
       </main>
